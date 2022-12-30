0aaf4e90e574f9174fcfcad858ddbf67306fc194
Kruno <kruno.zubrinic@gmail.com>
2022-12-30 14:29:46 +0100

    Prvi commit

---
+++ app/src/main/java/hr/unidu/kruno/kotlin/prvikotlinprimjer/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.unidu.kruno.kotlin.prvikotlinprimjer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
